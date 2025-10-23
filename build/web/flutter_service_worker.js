dc7e12c6470a675b15fad3ad17b13ef11cafebdd
John Kenneth U. Galicto <johnkennethgalicto@gmail.com>
2025-10-23 12:32:16 +0800

    want to update

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ffbff9f920d9293aaa5d7364ecca1bf0
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "ba347f53f280d48787f532a6ae563cbe",
+"flutter_bootstrap.js": "20cad71e395c2cd2ba6e2946e9fe2651",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
